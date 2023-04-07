ca05598be61b33ca0105795902768b0752311fd9
Portela <recupercao123@outlook.com>
2023-04-07 20:44:37 -0300

    Criação do Inimigo e Metodo de Seguir

--- Retrive/Assets/Scripts/Controllers/InimigoController.cs
+++ Retrive/Assets/Scripts/Controllers/InimigoController.cs
@@ -4,15 +4,32 @@ using UnityEngine;
 
 public class InimigoController : Inimigo
 {
-    // Start is called before the first frame update
+    [SerializeField] private Rigidbody2D myRB;
+    
+
     void Start()
     {
-        
+        myRB = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
+        SeguirPlayer();
+       
+    }
+
+    void SeguirPlayer()
+    {
+
+        Vector3 minhaposicao = transform.position;
+
+        Vector3 targetPosition = posPlayer.position;
+
+        Vector3 newPosition = Vector3.MoveTowards(minhaposicao, targetPosition, velocidadeMovimento * Time.deltaTime);
+
+        transform.position = newPosition;
+
     }
 }
--- Retrive/Assets/Scripts/Models/Inimigo.cs
+++ Retrive/Assets/Scripts/Models/Inimigo.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class Inimigo : EntidadeBase
 {
-  // Start is called before the first frame update
+    [SerializeField] protected Transform posPlayer;
     void Start()
     {
         
